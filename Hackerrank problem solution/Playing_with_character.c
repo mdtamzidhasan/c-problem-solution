80d1a6fec81234ad2ca3ca72e56a667321ff5dc9
tamzidhasan <tamzidhasan2020@gmail.com>
2022-08-19 23:02:07 +0600

    Plating with character

---
+++ Hackerrank problem solution/Playing_with_character.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ch;
+    char s[100],str[100];
+    scanf("%c ",&ch);
+    scanf("%s ",s);
+    gets(str);
+    printf("%c \n",ch);
+    printf("%s \n",s);
+    printf("%s \n",str);
+
+
+    return 0;
+}
