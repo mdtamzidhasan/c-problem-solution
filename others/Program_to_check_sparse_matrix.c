0bc54bd817f7cffd5c3a805399583d184d8aa698
tamzidhasan <tamzidhasan2020@gmail.com>
2022-09-12 19:27:41 +0600

    Program to check Sparse Matrix

---
+++ others/Program_to_check_sparse_matrix.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+   int r,c,sizer,sizec,total=0;
+   printf("Enter the Matrix Size:");
+   scanf("%d %d",&sizer,&sizec);
+   int A[sizer][sizec];
+   printf("Enter the element of this Matrix:\n");
+   for(r=0;r<sizer;r++)
+   {
+       for(c=0;c<sizec;c++)
+       {
+           scanf("%d",&A[r][c]);
+       }
+       printf("\n");
+   }
+   for(r=0;r<sizer;r++)
+   {
+       for(c=0;c<sizec;c++)
+       {
+           if(A[r][c]==0)
+            total++;
+       }
+   }
+   if(total>=(r*c)/2){
+     printf("This is a sparse Matrix!And this Matrix is :\n");
+    for(r=0;r<sizer;r++)
+    {
+        for(c=0;c<sizec;c++)
+        {
+            printf("%d",A[r][c]);
+        }
+        printf("\n");
+
+    }
+   }
+
+    else
+        printf("This is not sparse Matrix!");
+
+
+
+
+
+    return 0;
+}
