0fc5a4c838f1fe5b2644fed79aee152f6c56556d
tamzidhasan <tamzidhasan2020@gmail.com>
2022-09-13 23:40:54 +0600

    Program to find reverse of a string using pointer

---
+++ others/Program_to_find_reverse_of_a_string_using_pointers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+    char str[MAX_SIZE];
+    char rstr[MAX_SIZE];
+    char *s=str;
+    char *rs=rstr;
+    int length=0;
+    printf("Enter a string:");
+    gets(str);
+    while(*s){
+        length++;
+        s++;
+    }
+    s--;
+    while(length>=0)
+    {
+        *rs=*s;
+        rs++;
+        s--;
+        length--;
+    }
+    *rs='\0';
+    printf("Original string :%s\n",str);
+    printf("Reverse String :%s",rstr);
+
+    return 0;
+}
