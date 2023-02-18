0c6a5849c37a424a587fa48af645ea77c22bca99
unknown <Tamzid Hasan>
2023-02-18 21:16:11 +0600

    Duplicate Zeros

---
+++ leet code/leetcode problem solution/1089_Duplicate_Zeros.c
@@ -0,0 +1,30 @@
+void duplicateZeros(int* arr, int arrSize){
+int nums[arrSize];
+int i=0,j=0;
+while(i<arrSize&&j<arrSize)
+{
+    if(arr[i]==0)
+    {
+        nums[j]=0;
+        if(j+1<arrSize)
+        {
+            nums[j+1]=0;
+        }
+        j=j+2;
+        i++;
+
+    }
+    else
+    {
+        nums[j]=arr[i];
+        j++;
+        i++;
+    }
+
+}
+for(int k=0;k<arrSize;k++)
+{
+    arr[k]=nums[k];
+}
+return arr;
+}S
